5a7cc2e37400fea6c4009bf9609cf55bb29f7416
marcprux <mwp1@cornell.edu>
2024-01-31 09:57:19 -0500

    Disable STBTrueType

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     platforms: [.iOS(.v16), .macOS(.v13), .tvOS(.v16), .watchOS(.v9), .macCatalyst(.v16)],
     products: [
         .library(name: "SkipSTBImage", type: .dynamic, targets: ["SkipSTBImage"]),
-        .library(name: "SkipSTBTrueType", type: .dynamic, targets: ["SkipSTBTrueType"]),
+        //.library(name: "SkipSTBTrueType", type: .dynamic, targets: ["SkipSTBTrueType"]),
     ],
     dependencies: [
         .package(url: "https://source.skip.tools/skip.git", from: "0.7.42"),
@@ -34,20 +34,20 @@ let package = Package(
             .define("SKIP_BUILD_NDK") // needed for Skip to add native gradle build support
         ], plugins: [.plugin(name: "skipstone", package: "skip")]),
 
-        .target(name: "SkipSTBTrueType", dependencies: [
-            "STBTrueType",
-            .product(name: "SkipFoundation", package: "skip-foundation"),
-            .product(name: "SkipFFI", package: "skip-ffi")
-        ], plugins: [.plugin(name: "skipstone", package: "skip")]),
+        //.target(name: "SkipSTBTrueType", dependencies: [
+        //    "STBTrueType",
+        //    .product(name: "SkipFoundation", package: "skip-foundation"),
+        //    .product(name: "SkipFFI", package: "skip-ffi")
+        //], plugins: [.plugin(name: "skipstone", package: "skip")]),
 
-        .testTarget(name: "SkipSTBTrueTypeTests", dependencies: [
-            "SkipSTBTrueType",
-            .product(name: "SkipTest", package: "skip")
-        ], plugins: [.plugin(name: "skipstone", package: "skip")]),
+        //.testTarget(name: "SkipSTBTrueTypeTests", dependencies: [
+        //    "SkipSTBTrueType",
+        //    .product(name: "SkipTest", package: "skip")
+        //], plugins: [.plugin(name: "skipstone", package: "skip")]),
 
-        .target(name: "STBTrueType", sources: ["src"], cSettings: [
-            .define("SKIP_BUILD_NDK") // needed for Skip to add native gradle build support
-        ], plugins: [.plugin(name: "skipstone", package: "skip")]),
+        //.target(name: "STBTrueType", sources: ["src"], cSettings: [
+        //    .define("SKIP_BUILD_NDK") // needed for Skip to add native gradle build support
+        //], plugins: [.plugin(name: "skipstone", package: "skip")]),
 
     ]
 )
