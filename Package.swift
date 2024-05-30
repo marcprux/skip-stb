ed82804c14d19872851356e79fb1c200325adb36
marcprux <mwp1@cornell.edu>
2024-05-30 06:41:06 -0400

    Bump dependencies

--- Package.swift
+++ Package.swift
@@ -14,10 +14,10 @@ let package = Package(
         //.library(name: "SkipSTBTrueType", type: .dynamic, targets: ["SkipSTBTrueType"]),
     ],
     dependencies: [
-        .package(url: "https://source.skip.tools/skip.git", from: "0.7.42"),
-        .package(url: "https://source.skip.tools/skip-unit.git", from: "0.5.0"),
-        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.3.15"),
-        .package(url: "https://source.skip.tools/skip-ffi.git", from: "0.2.4")
+        .package(url: "https://source.skip.tools/skip.git", from: "0.8.46"),
+        .package(url: "https://source.skip.tools/skip-unit.git", from: "0.7.0"),
+        .package(url: "https://source.skip.tools/skip-foundation.git", from: "0.6.11"),
+        .package(url: "https://source.skip.tools/skip-ffi.git", from: "0.3.2")
     ],
     targets: [
         .target(name: "SkipSTBImage", dependencies: [
